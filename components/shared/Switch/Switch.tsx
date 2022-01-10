bce53e91ea956f1bd0b5da29906123862829be23
mimshins <mostafa.sh.coderino@gmail.com>
2022-01-10 17:08:29 +0330

    [:star:] add Switch component

---
+++ components/shared/Switch/Switch.tsx
@@ -0,0 +1,78 @@
+import useControlledProp from "@utilityjs/use-controlled-prop";
+import c from "classnames";
+import * as React from "react";
+import Text from "../Text";
+import classes from "./Switch.module.scss";
+
+interface SwitchBaseProps {
+  id: string;
+  label: string;
+  className?: string;
+  checked?: boolean;
+  defaultChecked?: boolean;
+  onChange?: (checked: boolean) => void;
+}
+
+type SwitchProps = Omit<
+  React.ComponentPropsWithRef<"div">,
+  keyof SwitchBaseProps
+> &
+  SwitchBaseProps;
+
+const SwitchBase = (props: SwitchProps, ref: React.Ref<HTMLDivElement>) => {
+  const {
+    label,
+    id,
+    checked,
+    defaultChecked,
+    onChange,
+    className,
+    ...otherProps
+  } = props;
+
+  const [isChecked, setIsChecked] = useControlledProp(
+    checked,
+    defaultChecked,
+    false
+  );
+
+  const emitChange = () => {
+    if (onChange) onChange(!isChecked);
+    setIsChecked(!isChecked);
+  };
+
+  return (
+    <div
+      id={id}
+      ref={ref}
+      className={c(className, classes.root, { [classes.active]: isChecked })}
+      {...otherProps}
+    >
+      <div
+        id={`${id}-input`}
+        role="switch"
+        aria-labelledby={`${id}-label`}
+        aria-checked={isChecked}
+        className={classes.input}
+        onClick={() => void emitChange()}
+      >
+        <div className={classes.indicator}></div>
+      </div>
+      <Text
+        as="label"
+        weight="medium"
+        variant="captionSmall"
+        onClick={() => void emitChange()}
+        id={`${id}-label`}
+        className={c(classes.label, "disable-user-select")}
+        htmlFor={`${id}-input`}
+      >
+        {label}
+      </Text>
+    </div>
+  );
+};
+
+const Switch = React.forwardRef(SwitchBase) as typeof SwitchBase;
+
+export default Switch;
