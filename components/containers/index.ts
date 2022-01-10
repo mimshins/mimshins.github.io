65892b5e62172a733ae1c542b370db73150fdf37
mimshins <mostafa.sh.coderino@gmail.com>
2022-01-10 17:11:15 +0330

    export new components

---
+++ components/containers/index.ts
@@ -0,0 +1,6 @@
+export { default as MainWrapper } from "./MainWrapper";
+export { default as Branding } from "./Branding";
+export { default as Nav } from "./Nav";
+export { default as DarkModeSwitch } from "./DarkModeSwitch";
+export { default as TitleSection } from "./TitleSection";
+export { default as Grids } from "./Grids";
