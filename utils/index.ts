bbbb1583dfcb3e1ce8cf159d79c7c47445bc0519
mimshins <mostafa.sh.coderino@gmail.com>
2022-02-10 14:25:49 +0330

    [:sparkles:] add utility fns

--- utils/index.ts
+++ utils/index.ts
@@ -3,3 +3,4 @@ export { default as setDescriptionMeta } from "./description-meta";
 export { default as isUndef } from "./is-undef";
 export { default as setKeywordsMeta } from "./keywords-meta";
 export { default as setTitleMeta } from "./title-meta";
+export { default as useIsomorphicLayoutEffect } from "./useIsomorphicLayoutEffect";
