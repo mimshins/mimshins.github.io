4a1c246cad87c4dba566f9214c8b83a7293d0ebb
mimshins <mostafa.sh.coderino@gmail.com>
2022-02-13 17:37:11 +0330

    add PWA support

--- next.config.js
+++ next.config.js
@@ -1,8 +1,15 @@
+const withPWA = require("next-pwa");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ["jsx", "js", "mdx", "md", "ts", "tsx"],
   reactStrictMode: true,
-  trailingSlash: false
+  trailingSlash: false,
+  pwa: {
+    dest: "public",
+    dynamicStartUrl: false,
+    disable: process.env.NODE_ENV === "development"
+  }
 };
 
-module.exports = nextConfig;
+module.exports = withPWA(nextConfig);
